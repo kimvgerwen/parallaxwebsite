d0a4e0329ab75aa97c75c2e5f610ec37999f6202
Kim van Gerwen <129595081+kimvgerwen@users.noreply.github.com>
2023-08-19 20:10:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let text = document.getElementById("text");
+
 let birds = document.getElementById("birds");
 let moon = document.getElementById("moon");
 let element6 = document.getElementById("6");
@@ -11,7 +11,6 @@ let element1 = document.getElementById("1");
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 + 'px';
     moon.style.top = value * -1.5 + 'px';
     moon.style.left = value * 1.5 + 'px';
     birds.style.top = value * -1.5 + 'px';
